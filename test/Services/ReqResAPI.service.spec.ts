cc2da1270e5673b13ad36375041e1e7936a319df
Dione Rodrigues Fernandes <dionefernandes@gmail.com>
2023-02-26 17:53:00 -0300

    File name update

--- test/Services/ReqResAPI.service.spec.ts
+++ test/Services/ReqResAPI.service.spec.ts
@@ -1,8 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HttpModule } from '@nestjs/common';
 import { ReqResAPIService } from '../../src/Services/ReqResAPI.service';
-import path from 'path';
-import * as fs from 'fs';
 
 describe('ReqResAPIService', () => {
   let service: ReqResAPIService;
@@ -18,16 +16,5 @@ describe('ReqResAPIService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
-  });
-
-  describe('downloadAvatar', () => {
-    it('should download and save the avatar image', async () => {
-      const avatarPath = 'https://reqres.in/img/faces/7-image.jpg';
-      await service.downloadAvatar(avatarPath);
-      const fileName = path.basename(avatarPath);
-      const dirPath = path.resolve(__dirname, '../../src/Assets/AvatarImg');
-      const filePath = `${dirPath}/${fileName}`;
-      expect(fs.existsSync(filePath)).toBeTruthy();
-    });
   });
 });
